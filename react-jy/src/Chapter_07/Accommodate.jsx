94afe5e43b161be7e2ea3511f94a7cd86973be4d
LAPTOP-E6PVK118\jongy <jongyong924@gmail.com>
2022-08-11 15:23:27 +0900

    React - hook(주석 추가)

--- react-jy/src/Chapter_07/Accommodate.jsx
+++ react-jy/src/Chapter_07/Accommodate.jsx
@@ -4,16 +4,22 @@ import useCounter from './useCounter';
 const MAX_CAPACITY = 10;
 
 function Accommodate(props){
+    // isFull state의 기본값을 false로 초기화
+    // setIfFull함수를 통해 값 변경
     const [isFull, setIsFull] = useState(false);
+    // 컴포넌트 사용
     const [count, increaseCount, decreaseCount] = useCounter(0);
 
+    // 컴포넌트 업데이트 시 마다 실행
     useEffect(() => {
         console.log("======================");
         console.log("useEffect() is called.");
         console.log(`isFull: ${isFull}`);
     });
 
+    // 의존 배열인 count의 값이 변할 때 실행
     useEffect(() => {
+        // setIsFull 함수 실행 조건
         setIsFull(count >= MAX_CAPACITY);
         console.log(`Current count value : ${count}`);
     }, [count]);
@@ -21,10 +27,10 @@ function Accommodate(props){
     return (
         <div style={{padding: 16}}>
             <p>{` 총 ${count}명 수용했습니다.`}</p>
-
+            {/* 이벤트 실행 시 컴포넌트 실행 */}
             <button onClick={increaseCount} disabled={isFull}>입장</button>
             <button onClick={decreaseCount}>퇴장</button>
-
+            
             {isFull && <p style={{color: "red"}}>정원이 가득찼습니다.</p>}
         </div>
     )
--- react-jy/src/Chapter_07/useCounter.jsx
+++ react-jy/src/Chapter_07/useCounter.jsx
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
 
 function useCounter(initialValue){
+    // 입력받은 param을 기본값으로 선언
     const [count, setCount] = useState(initialValue);
 
+    // 증가 함수 선언
     const increaseCount = () => setCount((count) => count + 1);
+    // 감소 함수 선언
     const decreaseCount = () => setCount((count) => Math.max(count - 1, 0));
 
+    // 변수, 함수를 리턴
     return [count, increaseCount, decreaseCount];
 }
 
